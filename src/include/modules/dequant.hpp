83347557611898b0948176ccd7344baa1dab4b1b
shdu <shdu@amd.com>
2026-06-18 17:07:28 -0700

    feat: update-mix-precision dequant header

--- src/include/modules/dequant.hpp
+++ src/include/modules/dequant.hpp
@@ -21,12 +21,17 @@ public:
     Dequant(LM_Config& config);
     ~Dequant();
 
-    /// @brief generate the dequant sequence
-    /// @param seq: the sequence
-    /// @param D_in: input dimension of the projection weight
-    /// @param D_out: output dimension of the projection weight
-    /// @param weight_offset: the weight offset in byte
-    /// @param mode: dequant output mode
+    typedef enum: int {
+        Q4_1 = 0,
+        Q8_0 = 1
+    } quant_block_t;
+
+    void reorder_cpy(u8 *dst, buffer<u8> &src,
+        quant_block_t quant_block_type,
+        const int quant_matrix_row,
+        const int quant_matrix_col,
+        const int vertical_blocks=2,
+        const int vetrical_block_interleave_byte_size=-1);
     void generate_dequant_q4_1_seq(npu_sequence* seq, const uint32_t D_in, const uint32_t D_out, const uint32_t weight_offset, int mode);
     void generate_dequant_q80_packed_in_q4nx_seq(npu_sequence* seq, const uint32_t D_in, const uint32_t D_out, const uint32_t weight_offset, int mode);
 private:

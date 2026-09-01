419e42466ad14f8c0611b8062475f6eae656bba0
zaneni6 <zani@amd.com>
2026-09-01 10:37:38 -0700

    fix/support: gemma4-it image max tokens (image budget)

--- src/include/AutoModel/modeling_gemma4_12b.hpp
+++ src/include/AutoModel/modeling_gemma4_12b.hpp
@@ -152,6 +152,20 @@ public:
                 return false;
             }
         }
+        else if (parameter_name == "image_max_tokens") {
+            try {
+                this->image_softtoken_budget = std::any_cast<int>(value);
+                
+                if(image_softtoken_budget != 70 && image_softtoken_budget != 140 &&
+                    image_softtoken_budget != 280 && image_softtoken_budget != 560 && image_softtoken_budget != 1120) {
+                    header_print("WARNING", "Invalid image budget value: " << image_softtoken_budget << ". Supported values are 70, 140, 280, 560, 1120. Using 280...");
+                    this->image_softtoken_budget = 280;
+                }
+                return true;
+            } catch (const std::bad_any_cast&) {
+                return false;
+            }            
+        }
 
         return false;
     }
